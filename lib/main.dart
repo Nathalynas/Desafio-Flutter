d2714e53ef1b59479849e6a85ffd64be0fdf34ea
Nathalynas <nathaly20camargo@gmail.com>
2025-04-14 12:35:23 -0300

    testes web firebase funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: avoid_print
+// ignore: avoid_web_libraries_in_flutter, deprecated_member_use
+import 'dart:js' as js;
 import 'package:almeidatec/providers/theme_provider.dart';
 import 'package:almeidatec/routes.dart';
 import 'package:almeidatec/services/firebase_notifications.dart';
@@ -19,26 +21,53 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
   print('🔕 Mensagem em segundo plano recebida: ${message.messageId}');
 }
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   if (kIsWeb) {
+    js.context.callMethod('eval', [
+      """
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('firebase-messaging-sw.js')
+        .then(function(registration) {
+          console.log('✅ Service Worker registrado:', registration.scope);
+        }).catch(function(err) {
+          console.log('❌ Erro ao registrar o Service Worker:', err);
+        });
+      }
+    """
+    ]);
+  }
+
+  if (kIsWeb) {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
-        apiKey: "AIzaSyCPLXk44ckhXVR0M3Cnc9qXAIkKA9bwjP0",
+        apiKey: "AIzaSyCPLXk44kchXVR0M3Cnc9qXAIkKA9bwjP0",
         authDomain: "almeidatec-c2c16.firebaseapp.com",
         projectId: "almeidatec-c2c16",
         storageBucket: "almeidatec-c2c16.appspot.com",
-        messagingSenderId: "54812456382",
-        appId: "1:54812456382:web:c174d24ab76786175c7faf",
+        messagingSenderId: "548121456382",
+        appId: "1:548121456382:web:c174d24ab76786175c7faf",
+        measurementId: "G-F5PDW45JL4",
       ),
     );
   } else {
     await Firebase.initializeApp();
   }
+  Future<void> printFcmToken() async {
+    try {
+      String? token = await FirebaseMessaging.instance.getToken();
+      print('🔑 Token FCM Web: $token');
+    } catch (e) {
+      print('❌ Erro ao obter token: $e');
+    }
+  }
+
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   await FirebaseNotificationService.initialize(MyApp.appStateKey);
-  
+  await printFcmToken();
+
   bool isLoggedIn = await AuthService.isUserLoggedInAndStayConnected();
 
   runApp(
@@ -54,7 +83,8 @@ void main() async {
 class MyApp extends StatefulWidget {
   final bool isLoggedIn;
 
-  static final GlobalKey<NavigatorState> appStateKey = GlobalKey<NavigatorState>();
+  static final GlobalKey<NavigatorState> appStateKey =
+      GlobalKey<NavigatorState>();
 
   const MyApp({super.key, required this.isLoggedIn});
 
@@ -63,11 +93,11 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
-  Locale _locale = const Locale('pt'); 
+  Locale _locale = const Locale('pt');
 
   void changeLanguage(Locale locale) {
     setState(() {
-      _locale = locale; 
+      _locale = locale;
     });
   }
 
@@ -75,10 +105,9 @@ class MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      scaffoldMessengerKey: scaffoldMessengerKey, 
+      scaffoldMessengerKey: scaffoldMessengerKey,
       navigatorKey: MyApp.appStateKey,
-
-      locale: _locale, 
+      locale: _locale,
       supportedLocales: const [
         Locale('en', ''),
         Locale('pt', ''),
@@ -90,8 +119,9 @@ class MyAppState extends State<MyApp> {
         GlobalCupertinoLocalizations.delegate,
       ],
       theme: Provider.of<ThemeProvider>(context).themeData,
-      initialRoute: widget.isLoggedIn ? Routes.productList : Routes.login, 
-      onGenerateRoute: (settings) => Routes.generateRoute(settings, changeLanguage),
+      initialRoute: widget.isLoggedIn ? Routes.productList : Routes.login,
+      onGenerateRoute: (settings) =>
+          Routes.generateRoute(settings, changeLanguage),
     );
   }
 }
--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,14 +1,27 @@
 importScripts("https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js");
 
-// Substitua pelos seus dados do Firebase
+
 firebase.initializeApp({
-  apiKey: "AIzaSyCPLxK44ckhXVR0M3Cnc9qXAIkKA9bwjP0",
+  apiKey: "AIzaSyCPLXk44kchXVR0M3Cnc9qXAIkKA9bwjP0",
   authDomain: "almeidatec-c2c16.firebaseapp.com",
   projectId: "almeidatec-c2c16",
   storageBucket: "almeidatec-c2c16.appspot.com",
-  messagingSenderId: "548124156382",
-  appId: "1:548124156382:web:c174d24ab76786175c7faf",
+  messagingSenderId: "548121456382",
+  appId: "1:548121456382:web:c174d24ab76786175c7faf",
+  measurementId: "G-F5PDW45JL4",
 });
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Mensagem em segundo plano: ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icons/icon-192.png', 
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
